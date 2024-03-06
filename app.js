44497fec2e9cb9a45cb3f382b6ddd9ca7ad5619a
Aram <aram.anto@gmail.com>
2024-03-06 14:47:52 +0100

    part 4 of the tutorial done

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
@@ -6,6 +7,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -15,7 +17,7 @@ mongoose.set("strictQuery", false);
 
 const mongoDB =
   "mongodb+srv://" +
-  process.env.USER +
+  process.env.USERNAME +
   ":" +
   process.env.SECRET +
   "@odin.q4mqzme.mongodb.net/local_library?retryWrites=true&w=majority&appName=Odin";
@@ -37,6 +39,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
